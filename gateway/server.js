b1d6ec5d35c9dfc34f8de6d9c382506ef12c3870
Romain Dauby <romain.dauby@orange.com>
2016-03-10 14:23:07 +0100

    Routing vers le nombre de lettres du prénom

--- gateway/server.js
+++ gateway/server.js
@@ -8,23 +8,36 @@ app
     .set('view engine', 'jade')
 ;
 
+function goData(res, resource) {
+    resource.on("data", function (chunk) {
+        res.write(chunk);
+    }).on('end', function () {
+        res.end();
+    });
+}
+
 app
     .get('/', function (req, res) {
         res.render('index.jade', {env: env});
     })
     .get('/prenom/:prenom', function (req, res) {
-        http.get("http://prenom:7001/" + req.params.prenom, function(resource) {
-            var content = "";
-            resource.on("data", function(chunk) {
-                content += chunk;
-            });
-            res.json({foo: content});
+        http.get("http://prenom:7001/" + req.params.prenom, function (resource) {
+            goData(res, resource);
+        }).on('error', function (e) {
+            console.log(e.message);
+        });
+    })
+    .get('/prenom/nombreDeLettre/:prenom', function (req, res) {
+        http.get("http://prenom:7001/nombreDeLettre/" + req.params.prenom, function (resource) {
+            goData(res, resource);
+        }).on('error', function (e) {
+            console.log(e.message);
         });
     })
 ;
 
 app
-    .use(function(req, res){
+    .use(function (req, res) {
         res.redirect('/');
     })
 ;
